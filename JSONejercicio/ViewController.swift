c1248bd40fd68ed317b92bf0f7e5d7797115174d
Pau Enrech <pauenrech@paus-imac.local>
2019-03-11 12:37:02 +0100

    Initial Commit

---
+++ JSONejercicio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONejercicio
+//
+//  Created by Pau Enrech on 11/03/2019.
+//  Copyright © 2019 Pau Enrech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
