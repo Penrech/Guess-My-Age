b7345d36bfaaeca431647359a825fdacc79fc60e
pauenrech <pauenrech94@gmail.com>
2019-09-30 13:46:34 +0200

    Minor fixes

--- JSONejercicio/FaceApi.swift
+++ JSONejercicio/FaceApi.swift
@@ -22,13 +22,12 @@ struct FaceApi {
     let scaleWidthToNormalize: CGFloat?
     
     private init() {
-        let apiKey: String = String()
         self.scaleWidthToNormalize = 1000
         self.location = "westeurope"
         self.returnFaceId = false
         self.returnFaceLandmarks = false
         self.returnFaceAttributes = "age,gender,smile,facialHair,glasses,emotion,hair,makeup,accessories"
-        self.apiKey = apiKey.getApiKey()
+        self.apiKey = String.getApiKey()
         self.stringUrl = "https://\(self.location!).api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=\(self.returnFaceId!)&returnFaceLandmark=\(self.returnFaceLandmarks!)&returnFaceAttributes=\(self.returnFaceAttributes!)"
     }
     
